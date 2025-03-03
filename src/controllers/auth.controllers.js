da1378bbb55884ccd01e286153570cd595e0b667
rohit negi <rohitnegi263139@gmail.com>
2025-03-03 17:30:31 +0530

    add encrypt password and jwt authentication for signup

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../middlewares/generateToken.js";
 
 export const signup = async (req, res) => {
 	try {
@@ -13,14 +15,19 @@ export const signup = async (req, res) => {
 			throw new Error("User already exist!");
 		}
 
+		const salt = await bcrypt.genSalt(10);
+		const passwordHash = await bcrypt.hash(password, salt);
 		const user = new User({
 			firstName,
 			lastName,
 			email,
 			age,
-			password,
+			password: passwordHash,
 		});
+
 		await user.save();
+		const token = generateToken(res, user._id);
+		res.cookie("jwt_token", token, { secure: true, httpOnly: true });
 		res.status(201).send(user);
 	} catch (error) {
 		res.status(400).send("ERROR : " + error.message);
